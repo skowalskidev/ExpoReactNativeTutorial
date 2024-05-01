2abf83eedeeb5dac4fdbccab10bf28f871d89d8f
skowalskidev <skowalskidev@gmail.com>
2024-05-01 22:05:37 +0700

    add lists

--- App.js
+++ App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { View, Text, Image, ScrollView, TextInput } from 'react-native';
 import Cafe from './src/components/Cafe';
 import PizzaTranslator from './src/components/PizzaTranslator';
+import FlatListBasics from './src/components/FlatListBasics';
+import SectionListBasics from './src/components/SectionListBasics';
 
 const App = () => {
   return (
-    <ScrollView style={{
-      paddingTop: 40,
-      paddingBottom: 40,
-      paddingLeft: 10,
-      paddingRight: 10,
-    }}>
+    <>
       <View style={{
+        paddingTop: 40,
+        // paddingBottom: 40,
+        paddingLeft: 10,
+        paddingRight: 10,
         gap: 20,
       }}>
         <Text style={{ textAlign: 'center' }}>Welcome to the cafe!</Text>
         <Cafe />
         <PizzaTranslator />
       </View>
-    </ScrollView>
+      {/* <FlatListBasics /> */}
+      <SectionListBasics />
+    </>
   );
 };
 
